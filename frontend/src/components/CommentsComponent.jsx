2afa5d86d2b60e25ea39e93c5fe85156bbab482a
expiredDairy <164272300+expiredDairy@users.noreply.github.com>
2024-07-03 19:30:09 +0300

    yy

--- frontend/src/components/CommentsComponent.jsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export const CommentsComponent = () => {
-  return <div>CommentsComponent</div>;
-};
--- frontend/src/components/LikesComponent.jsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export const LikesComponent = () => {
-  return <div>LikesComponent</div>;
-};
